11ad8128f0ad78810101b325cb0519da510f6c99
rustamovjavohir <rustamovj366@gmail.com>
2023-08-01 10:20:10 +0500

    add 500 error template

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from config import settings
 
-urlpatterns = [
+urlpatterns = ([
                   path('admin/', admin.site.urls),
                   path('api/', include('api.urls')),
                   path('', include('apps.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
 handler500 = 'apps.views.error_500'
 handler404 = 'apps.views.error_404'
