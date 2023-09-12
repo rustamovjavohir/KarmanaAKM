d37aae80ffbb9bf46dcd205ea83541102930b9dc
rustamovjavohir <rustamovj366@gmail.com>
2023-09-12 20:26:14 +0500

    Add monitoring middleware

--- utils/middlewares.py
+++ utils/middlewares.py
@@ -9,7 +9,7 @@ class VisitorMiddleware:
     def __call__(self, request):
         # Log visitor activity here
         # Example: Create a Visit record in your database
-        Visitors.objects.update_or_create(ip_address=request.META.get('REMOTE_ADDR'),
+        Visitors.objects.update_or_create(ip_address=self.get_client_ip(request),
                                           defaults={
                                               'user_agent': request.META.get('HTTP_USER_AGENT'),
                                               'path': request.path,
@@ -18,6 +18,13 @@ class VisitorMiddleware:
                                               'query_string': request.META.get('QUERY_STRING'),
                                               'updated_at': timezone.now(),
                                           })
-
         response = self.get_response(request)
         return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
