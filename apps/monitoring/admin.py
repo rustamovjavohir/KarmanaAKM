3780a940f06f427dccc7445336dee7445e56fab1
rustamovjavohir <rustamovj366@gmail.com>
2023-09-12 22:55:49 +0500

    Add monitoring middleware

--- apps/monitoring/admin.py
+++ apps/monitoring/admin.py
@@ -6,3 +6,10 @@ from apps.monitoring.models import Visitors
 class VisitorsAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'user_agent', 'path', 'created_at', 'updated_at')
     list_display_links = ('ip_address', 'path')
+    actions = ('get_extra_datas',)
+
+    def get_extra_datas(self, request, queryset):
+        for obj in queryset:
+            obj.fill_data()
+
+    get_extra_datas.short_description = 'Дополнительные данные'
