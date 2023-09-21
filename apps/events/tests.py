c724b410e9f1abcbbb66a3af5566abf55f9219f4
rustamovjavohir <rustamovj366@gmail.com>
2023-09-21 13:02:32 +0500

    add CI

--- apps/events/tests.py
+++ apps/events/tests.py
@@ -1,6 +1,6 @@
-from django.test import TestCase
-
-
-class TestEvents(TestCase):
-    def test_events(self):
-        self.assertEqual(1, 1)
+# from django.test import TestCase
+#
+#
+# class TestEvents(TestCase):
+#     def test_events(self):
+#         self.assertEqual(1, 1)
