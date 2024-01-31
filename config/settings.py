15780368499142f83b7ebdc5de36aef778fbc6a2
rustamovjavohir <rustamovj366@gmail.com>
2024-01-31 14:38:10 +0500

    change url

--- config/settings.py
+++ config/settings.py
@@ -68,12 +68,14 @@ THIRD_APP = [
     'rest_framework.authtoken',
     'ckeditor',
     'django_filters',
+    'whitenoise',
 ]
 
 INSTALLED_APPS = DEFAULT_APPS + LOCAL_APPS + THIRD_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
