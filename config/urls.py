286a69e6e0dfa703c303e20ed7a5d0fc43bf488a
rustamovjavohir <rustamovj366@gmail.com>
2024-01-31 14:32:28 +0500

    change url

--- config/urls.py
+++ config/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from config import settings
 
-urlpatterns = ([
+urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/', include('api.urls')),
                   path('', include('apps.urls'))
+                  re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+                  re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
 handler500 = 'apps.views.error_500'
 handler404 = 'apps.views.error_404'
